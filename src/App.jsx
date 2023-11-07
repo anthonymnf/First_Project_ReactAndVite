76cf92f57840c27d13f45ca27f0bca8536892d8e
anthonymnf <anthonymnf30@gmail.com>
2023-11-07 10:15:30 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <h1>Olá Mundo</h1>
+      <p>Este é meu primeiro projeto usando React e Vite!</p>
+      <p>Próximos passos:</p>
+      <ul>
+        <li>Praticar com exercícios</li>
+        <li>Aprender a usar dados dinâmicos</li>
+        <li>Criar novos componentes</li>
+        <li>Estilizar os componentes</li>
+        <li>Reutilizar componentes</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
